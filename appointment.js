e3d00d8a55ba64a4ba17728571f380d176140bc1
Sterben889 <59096329+Sterben889@users.noreply.github.com>
2022-12-06 21:25:06 -0600

    Finished dentist website

--- appointment.js
+++ appointment.js
@@ -1,3 +1,14 @@
 var today = new Date().toISOString().slice(0, 16);
 
 document.getElementsByName("book")[0].min = today;
+
+send(){
+  document.getElementById('theForm').submit();
+});
+function keypresshandler(event)
+   {
+        var charCode = event.keyCode;
+        //Non-numeric character range
+        if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+   }
